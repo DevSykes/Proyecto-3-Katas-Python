d17d6fbac50d58ee2eeedc12ff9c56f8e24b5fdb
S.Z. <234208376+DevSykes@users.noreply.github.com>
2026-05-12 23:22:36 +0200

    enunciado ejercicio 8

--- katas.py
+++ katas.py
@@ -95,7 +95,7 @@ def lista_strings(lista_tuplas):
 # 8. Escribe un programa que pida al usuario dos números e intente dividirlos.
 # Si el usuario ingresa un valor no numérico o intenta dividir por cero, 
 # maneja esas excepciones de manera adecuada y muestra un mensaje indicando
-# si la división fue exitosa o no
+# si la división fue exitosa o no.
 # ==============================================================================
 
 
